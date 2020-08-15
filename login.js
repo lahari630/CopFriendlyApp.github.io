efdc67c16ea59cca655d1f267ee29dff2e877593
lahari630 <58872652+lahari630@users.noreply.github.com>
2020-08-15 21:20:40 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,27 @@
+ function login()
+	{
+		var uname = document.getElementById("email").value;
+		var pwd = document.getElementById("pwd1").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("please enter user name.");
+		}
+		else if(pwd=='')
+		{
+        	alert("enter the password");
+		}
+		else if(!filter.test(uname))
+		{
+			alert("Enter valid email id.");
+		}
+		
+		else
+		{
+	alert('Thank You for Login & You are Redirecting to Campuslife Website');
+  //Redirecting to other page or webste code or you can set your own html page.
+       <a class="more_btn" href="TrafficCop.html">
+	 
+			}
+	}		
+	
